498bd7ee812f06a271d549d5dca3763edffb1d4c
mujeebahmad <abdulmujeebahmad03@gmail.com>
2025-09-03 11:47:18 +0100

    Remove unused imports from teams.module.ts to streamline module dependencies.

--- server/src/teams/teams.module.ts
+++ server/src/teams/teams.module.ts
@@ -1,11 +1,8 @@
 import { Module } from "@nestjs/common";
 import { TeamsService } from "./teams.service";
 import { TeamsController } from "./teams.controller";
-import { PrismaModule } from "src/prisma/prisma.module";
-import { HelperModule } from "src/helper/helper.module";
 
 @Module({
-  imports: [PrismaModule, HelperModule],
   controllers: [TeamsController],
   providers: [TeamsService],
   exports: [TeamsService],
