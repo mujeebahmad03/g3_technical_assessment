189c7b018dd1e8d3c6ba15994f35936bd100e278
mujeebahmad <abdulmujeebahmad03@gmail.com>
2025-09-06 03:28:09 +0100

    Remove PrismaBaseService and PrismaQueryService from the project
    - Deleted PrismaBaseService and PrismaQueryService files to streamline the codebase.
    - Updated PrismaModule to only provide PrismaService, removing unnecessary dependencies and exports.

--- server/src/prisma/prisma.module.ts
+++ server/src/prisma/prisma.module.ts
@@ -1,22 +1,9 @@
 import { Global, Module } from "@nestjs/common";
-
 import { PrismaService } from "./prisma.service";
-import { PrismaQueryService } from "./prisma-query.service";
-import { PrismaBaseService } from "./prisma-base.service";
 
 @Global()
 @Module({
-  providers: [
-    PrismaService,
-    PrismaQueryService,
-    {
-      provide: PrismaBaseService,
-      useFactory: (prisma: PrismaService) => {
-        return new PrismaBaseService(prisma);
-      },
-      inject: [PrismaService],
-    },
-  ],
-  exports: [PrismaService, PrismaQueryService, PrismaBaseService],
+  providers: [PrismaService],
+  exports: [PrismaService],
 })
 export class PrismaModule {}
